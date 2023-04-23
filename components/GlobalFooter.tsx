999217d90552639ddb02eccda75a928f9eaf3b8c
Seven Yu <dofyyu@gmail.com>
2023-04-23 17:51:12 +0800

    update components

--- components/GlobalFooter.tsx
+++ components/GlobalFooter.tsx
@@ -6,13 +6,13 @@ const GlobalFooter: React.FC = () => {
   const year = new Date().getFullYear()
   const { version } = package_json
   return (
-    <Footer background="light-3" justify="between" pad="medium">
+    <Footer background="light-3" justify="between" pad="large">
       <Text color="dark-3" size="small">
-        copyright &copy; {process.env.NEXT_PUBLIC_SINCE}~{year} - powered by
-        yahaha.net
+        {`copyright (c) ${process.env.NEXT_PUBLIC_SINCE} ~ ${year}
+        powered by yahaha.net`}
       </Text>
       <Text color="dark-6" size="small" weight="bold">
-        ( version {version} )
+        {`( version ${version} )`}
       </Text>
     </Footer>
   )
--- components/StepCard.tsx
+++ components/StepCard.tsx
@@ -1,21 +1,72 @@
-import { Card } from 'grommet'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+} from 'grommet'
 
 interface StepCardProps {
   isFirst: boolean
   isLast: boolean
   step: number
+  canGoBack?: boolean
+  onPrevious?: () => void
+  onNext?: () => void
   onCompleted?: () => void
   children: React.ReactNode
+  previousLabel?: string
+  nextLabel?: string
+  completedLabel?: string
 }
 
 const StepCard: React.FC<StepCardProps> = ({
   isFirst,
   isLast,
   step,
-  children,
+  canGoBack = false,
+  onPrevious,
+  onNext,
   onCompleted,
+  children,
+  previousLabel = 'Previous',
+  nextLabel = 'Next',
+  completedLabel = 'Completed',
 }) => {
-  return <Card>{children}</Card>
+  return (
+    <Card>
+      <CardHeader
+        pad={{ horizontal: 'large', vertical: 'none' }}
+        background={{ dark: 'dark-3', light: 'light-5' }}
+      >
+        <Heading level={3}>{`STEP ${step} :`}</Heading>
+      </CardHeader>
+      <CardBody pad="medium">{children}</CardBody>
+      {(onPrevious || onNext || (isLast && onCompleted)) && (
+        <CardFooter
+          pad="medium"
+          background={{ dark: 'dark-6', light: 'light-3' }}
+        >
+          {canGoBack && onPrevious ? (
+            <Button
+              label={previousLabel}
+              disabled={isFirst}
+              onClick={onPrevious}
+            />
+          ) : (
+            <></>
+          )}
+          {!isLast && onNext && (
+            <Button primary label={nextLabel} onClick={onNext} />
+          )}
+          {isLast && onCompleted && (
+            <Button primary label={completedLabel} onClick={onCompleted} />
+          )}
+        </CardFooter>
+      )}
+    </Card>
+  )
 }
 
 export default StepCard
