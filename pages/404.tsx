c73701cf90ae48e785a17597dfee7b9f07a6dbec
Seven Yu <dofyyu@gmail.com>
2023-04-23 17:50:12 +0800

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import GlobalPage from '@/components/GlobalPage'
+import { Box, Button, Heading, Text } from 'grommet'
+
+const Page = () => {
+  return (
+    <GlobalPage>
+      <Box align="center">
+        <Heading>404</Heading>
+        <Text>Page Not Found.</Text>
+        <Box pad="large">
+          <Button primary label="Go Back" onClick={() => history.back()} />
+        </Box>
+      </Box>
+    </GlobalPage>
+  )
+}
+
+export default Page
