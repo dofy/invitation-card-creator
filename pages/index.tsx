8da0a05ece0352f00e8532bc3543f95c4a6ab13b
Seven Yu <dofyyu@gmail.com>
2023-04-23 17:51:00 +0800

    update index page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,49 @@
-import GlobalFooter from '@/components/GlobalFooter'
-import GlobalHead from '@/components/GlobalHead'
+import GlobalPage from '@/components/GlobalPage'
 import StepByStep from '@/components/StepByStep'
 import StepCard from '@/components/StepCard'
-import { Box, Main, Page, PageContent, PageHeader } from 'grommet'
+import { Grid, Notification } from 'grommet'
 import React from 'react'
 
 export default function Home() {
   const totalSteps = 7
   const [step, setStep] = React.useState(3)
+  const [showNotification, setShowNotification] = React.useState(false)
 
   return (
-    <React.Fragment>
-      <GlobalHead />
-      <Page>
-        <PageContent>
-          <PageHeader
-            title={process.env.NEXT_PUBLIC_TITLE}
-            subtitle={process.env.NEXT_PUBLIC_DESC}
-          />
-          <Main>
-            <Box>
-              <Box>
-                <StepByStep
-                  step={step}
-                  totalSteps={totalSteps}
-                  onStep={setStep}
-                />
-              </Box>
-              <Box>
-                <StepCard
-                  isFirst={step === 1}
-                  isLast={step === totalSteps}
-                  step={step}
-                  onCompleted={() => setStep(step + 1)}
-                >
-                  content...
-                </StepCard>
-              </Box>
-              <Box>--== STEPS ==--</Box>
-              <Box>step 1: Upload Background Image</Box>
-              <Box>step 2: Set Name Position & Font Size and Preview</Box>
-              <Box>step 3: Upload Names-List file</Box>
-              <Box>中文测试，中华人民共和国</Box>
-            </Box>
-          </Main>
-        </PageContent>
-        <GlobalFooter />
-      </Page>
-    </React.Fragment>
+    <GlobalPage>
+      <Grid
+        rows={[
+          'auto', // StepByStep
+          'flex', // StepCard
+        ]}
+        gap="medium"
+      >
+        <StepByStep step={step} totalSteps={totalSteps} onStep={setStep} />
+        <StepCard
+          isFirst={step === 1}
+          isLast={step === totalSteps}
+          step={step}
+          canGoBack={true}
+          onNext={() => setStep(step + 1)}
+          onCompleted={() => setShowNotification(true)}
+        >
+          content...
+        </StepCard>
+        {/* <Box>--== STEPS ==--</Box>
+        <Box>step 1: Upload Background Image</Box>
+        <Box>step 2: Set Name Position & Font Size and Preview</Box>
+        <Box>step 3: Upload Names-List file</Box>
+        <Box>中文测试，中华人民共和国</Box> */}
+      </Grid>
+      {showNotification && (
+        <Notification
+          toast
+          title="Notification Title"
+          message="Notification Message"
+          status="info"
+          onClose={() => setShowNotification(false)}
+        />
+      )}
+    </GlobalPage>
   )
 }
