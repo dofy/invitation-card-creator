8e2f12c374b126f9cbf2e471273aa64fedba2835
Seven Yu <dofyyu@gmail.com>
2023-04-23 18:46:31 +0800

    fix page header

--- components/GlobalPage.tsx
+++ components/GlobalPage.tsx
@@ -1,6 +1,7 @@
 import GlobalFooter from '@/components/GlobalFooter'
 import GlobalHead from '@/components/GlobalHead'
-import { Main, Page, PageContent, PageHeader } from 'grommet'
+import { Box, Heading, Main, Page, PageContent } from 'grommet'
+import { UserAdd } from 'grommet-icons'
 import React from 'react'
 
 interface GlobalPageProps {
@@ -13,10 +14,35 @@ const GlobalPage: React.FC<GlobalPageProps> = ({ children }) => {
       <GlobalHead />
       <Page>
         <PageContent>
-          <PageHeader
-            title={process.env.NEXT_PUBLIC_TITLE}
-            subtitle={process.env.NEXT_PUBLIC_DESC}
-          />
+          <Box pad="small">
+            <Box gap="small" direction="row" align="baseline">
+              <UserAdd size="large" color="brand" />
+              <Heading
+                margin={{
+                  horizontal: 'medium',
+                  bottom: 'small',
+                  top: 'large',
+                }}
+                color="brand"
+                level={1}
+              >
+                {process.env.NEXT_PUBLIC_TITLE}
+              </Heading>
+            </Box>
+            <Heading
+              color={{
+                light: 'dark-3',
+                dark: 'light-3',
+              }}
+              margin={{
+                left: 'large',
+                vertical: 'small',
+              }}
+              level={4}
+            >
+              {process.env.NEXT_PUBLIC_DESC}
+            </Heading>
+          </Box>
           <Main pad="large">
             {/* page content begin */}
             {children}
