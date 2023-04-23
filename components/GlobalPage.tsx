da4e71bd93ed9bf20943d88a17fe4ad0d1344d91
Seven Yu <dofyyu@gmail.com>
2023-04-23 18:55:51 +0800

    add GlobalPageHeader component

--- components/GlobalPage.tsx
+++ components/GlobalPage.tsx
@@ -1,8 +1,9 @@
 import GlobalFooter from '@/components/GlobalFooter'
 import GlobalHead from '@/components/GlobalHead'
-import { Box, Heading, Main, Page, PageContent } from 'grommet'
+import { Main, Page, PageContent } from 'grommet'
 import { UserAdd } from 'grommet-icons'
 import React from 'react'
+import GlobalPageHeader from './GlobalPageHeader'
 
 interface GlobalPageProps {
   children: React.ReactNode
@@ -14,35 +15,11 @@ const GlobalPage: React.FC<GlobalPageProps> = ({ children }) => {
       <GlobalHead />
       <Page>
         <PageContent>
-          <Box pad="small">
-            <Box gap="small" direction="row" align="baseline">
-              <UserAdd size="large" color="brand" />
-              <Heading
-                margin={{
-                  horizontal: 'medium',
-                  bottom: 'small',
-                  top: 'large',
-                }}
-                color="brand"
-                level={1}
-              >
-                {process.env.NEXT_PUBLIC_TITLE}
-              </Heading>
-            </Box>
-            <Heading
-              color={{
-                light: 'dark-3',
-                dark: 'light-3',
-              }}
-              margin={{
-                left: 'large',
-                vertical: 'small',
-              }}
-              level={4}
-            >
-              {process.env.NEXT_PUBLIC_DESC}
-            </Heading>
-          </Box>
+          <GlobalPageHeader
+            icon={<UserAdd size="large" color="brand" />}
+            title={process.env.NEXT_PUBLIC_TITLE}
+            subTitle={process.env.NEXT_PUBLIC_DESC}
+          />
           <Main pad="large">
             {/* page content begin */}
             {children}
---
+++ components/GlobalPageHeader.tsx
@@ -0,0 +1,43 @@
+import { Box, Heading } from 'grommet'
+
+interface GlobalPageHeaderProps {
+  icon?: React.ReactNode
+  title?: string
+  subTitle?: string
+}
+
+const GlobalPageHeader: React.FC<GlobalPageHeaderProps> = ({
+  title,
+  icon,
+  subTitle,
+}) => {
+  return (
+    <Box pad="medium">
+      <Box gap="small" direction="row" align="baseline">
+        {icon && icon}
+        <Heading
+          margin={{
+            horizontal: 'medium',
+            bottom: 'small',
+            top: 'large',
+          }}
+          color="brand"
+          level={1}
+        >
+          {title}
+        </Heading>
+      </Box>
+      {subTitle && (
+        <Heading
+          color={{ light: 'dark-3', dark: 'light-3' }}
+          margin={{ left: 'large', vertical: 'small' }}
+          level={4}
+        >
+          {subTitle}
+        </Heading>
+      )}
+    </Box>
+  )
+}
+
+export default GlobalPageHeader
