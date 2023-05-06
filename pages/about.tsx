914044f177aeca9d5c6ec4e5d493fccf3138629b
Seven Yu <dofyyu@gmail.com>
2023-05-06 13:25:09 +0800

    update packages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,18 @@
 import GlobalPage from '@/components/GlobalPage'
-import { Box, Button } from 'grommet'
+import { Box, Button, Header } from 'grommet'
 import React from 'react'
 
 interface IPageProps {}
 
 const Page: React.FC<IPageProps> = (props) => {
-  return <GlobalPage>About</GlobalPage>
+  return (
+    <GlobalPage>
+      <Header>About</Header>
+      <Box pad="medium">
+        <Button label="Click me" onClick={() => alert('Clicked!')} />
+      </Box>
+    </GlobalPage>
+  )
 }
 
 export default Page
